09fa093fadf95a565f70c0aec63a69a9578785eb
NhanNTWPV <hostdknhan2@gmail.com>
2023-02-12 23:16:09 +0700

    Create restore-bitwarden.sh

---
+++ restore-bitwarden.sh
@@ -0,0 +1,22 @@
+snap remove google-cloud-sdk
+snap install google-cloud-cli --classic
+gcloud init
+
+FILEPATH=gs://btwrdn-backup/$1
+gcloud storage ls $FILEPATH
+FILE_EXISTS=$?
+
+if [[ $FILE_EXISTS == 1 ]]; then
+  echo "$1 not found on the storage account. Exiting..."
+  exit 1
+fi
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+curl -Lso bitwarden.sh https://go.btwrdn.co/bw-sh && chmod 700 bitwarden.sh
+gcloud storage cp $FILEPATH $1
+./bitwarden.sh start
+
+systemctl enable cron
+crontab -l | { cat; echo "0 1,9,17 * * * $(pwd)/backup-bitwarden.sh >/dev/null 2>&1"; } | crontab -
+rm -- "$0"
