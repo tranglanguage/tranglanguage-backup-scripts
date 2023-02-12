3c15d99ee30e46b8f7db4d83553552f6b69a4297
NhanNTWPV <hostdknhan2@gmail.com>
2023-02-12 23:29:08 +0700

    Update restore-bitwarden.sh

--- restore-bitwarden.sh
+++ restore-bitwarden.sh
@@ -15,6 +15,7 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 curl -Lso bitwarden.sh https://go.btwrdn.co/bw-sh && chmod 700 bitwarden.sh
 gcloud storage cp $FILEPATH $1
+unzip $1
 ./bitwarden.sh start
 
 systemctl enable cron
