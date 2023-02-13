9f4bc3d0d5855e3ef04a0ea3d5a209564276022d
NhanNTWPV <hostdknhan2@gmail.com>
2023-02-14 00:24:06 +0700

    Update backup-bitwarden.sh

--- backup-bitwarden.sh
+++ backup-bitwarden.sh
@@ -4,7 +4,7 @@ cd $DIR
 
 ./bitwarden.sh stop
 BACKUPNAME=$DIR/$(date +%F_%H-%M-%S).zip
-zip -r $BACKUPNAME $DIR/bwdata -x $DIR/bwdata/mssql/backups/**\* $DIR/bwdata/letsencrypt/**\*
+zip -r $BACKUPNAME bwdata -x bwdata/mssql/backups/**\* bwdata/letsencrypt/**\*
 /snap/bin/gcloud storage cp $BACKUPNAME gs://btwrdn-backup/
 rm -rf $BACKUPNAME
 ./bitwarden.sh start
